d00796402421e2d33dfe4891909081c4cfd5beec
gautham <gauthamg425@gmail.com>
2025-04-23 22:55:50 +0530

    .

--- jenkins_helper.py
+++ jenkins_helper.py
@@ -1,4 +1,23 @@
 
+import os
+import json
+
+TRACKER_FILE = "retry_tracker.json"
+NODE_TRACKER_FILE = "node_tracker.json"
+
+def load_tracker(file):
+    if os.path.exists(file):
+        with open(file, "r") as f:
+            return json.load(f)
+    return {}
+
+def save_tracker(file, data):
+    with open(file, "w") as f:
+        json.dump(data, f, indent=2)
+
+retry_counts = load_tracker(TRACKER_FILE)
+node_retry_counts = load_tracker(NODE_TRACKER_FILE)
+
 def safe_get_json(resp, url):
     try:
         if resp.headers.get('Content-Type', '').startswith('application/json'):
@@ -56,6 +75,7 @@ data = safe_get_json(resp, ")")
 
 def retry_job(view, job_name):
     retry_counts[job_name] = retry_counts.get(job_name, 0) + 1
+    save_tracker(TRACKER_FILE, retry_counts)
     if retry_counts[job_name] <= 3:
         requests.post(f"{JENKINS_URL}/view/{view}/job/{job_name}/build", **REQUEST_KWARGS)
         return f"Triggered retry for {job_name} (Attempt {retry_counts[job_name]}/3)"
@@ -76,6 +96,7 @@ def get_nodes():
 
 def reconnect_node(node_name):
     node_retry_counts[node_name] = node_retry_counts.get(node_name, 0) + 1
+    save_tracker(NODE_TRACKER_FILE, node_retry_counts)
     if node_retry_counts[node_name] <= 3:
         return f"Reconnect attempted for {node_name} (Mocked Attempt {node_retry_counts[node_name]}/3)"
     else:
